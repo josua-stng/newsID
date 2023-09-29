6a750f97717d5fe4bd2b421192def730b938b056
josua-stng <josuasitanggang51@gmail.com>
2023-09-29 19:24:34 +0700

    add:image source

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+        domains:['akcdn.detik.net.id','awsimages.detik.net.id','asset-2.tstatic.net','img.okezone.com']
+    }
+}
 
 module.exports = nextConfig
