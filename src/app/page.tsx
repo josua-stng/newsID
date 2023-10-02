3658d1d7489d0b63b094576512e2a0902256fb22
josua-stng <josuasitanggang51@gmail.com>
2023-10-02 19:25:18 +0700

    fix:page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import News from './components/news';
-import Navbar from './header/navbar';
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <News />
     </div>
   );
