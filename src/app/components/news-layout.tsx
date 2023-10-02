2bb03257c3dd371940464b63a05803fa53491940
josua-stng <josuasitanggang51@gmail.com>
2023-10-02 19:23:49 +0700

    fix:layout news design

--- src/app/components/news-layout.tsx
+++ src/app/components/news-layout.tsx
@@ -1,20 +1,28 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
-export default function NewsLayout({ image, description, date }: any) {
+export default function NewsLayout({
+  newsLink,
+  image,
+  description,
+  date,
+}: any) {
   return (
-    <div className="max-w-xs md:max-w-sm lg:max-w-md mx-auto border-2 border-gray-300 mb-5 p-5 font-sans cursor-pointer hover:mb-[-2px]">
-      <Image
-        alt="news-image"
-        width={280}
-        height={150}
-        src={image}
-        style={{ objectFit: 'cover' }}
-        className="mx-auto pb-5"
-      />
-      <p className="line-clamp-2 text-">{description}</p>
-      <p className="mt-2 bg-green-200 max-w-max px-2 py-1 rounded-sm font-semibold">
-        {new Date(date).toISOString().split('T')[0]}
-      </p>
-    </div>
+    <Link href={newsLink} target='_blank'>
+      <div className="max-w-xs md:max-w-sm lg:max-w-md mx-auto border-2 border-gray-300 mb-5 p-5 font-sans cursor-pointer hover:mb-[-2px]">
+        <Image
+          alt="news-image"
+          width={280}
+          height={150}
+          src={image}
+          style={{ objectFit: 'cover' }}
+          className="mx-auto pb-5"
+        />
+        <p className="line-clamp-2 text-">{description}</p>
+        <p className="mt-2 bg-green-200 max-w-max px-2 py-1 rounded-sm font-semibold">
+          {new Date(date).toISOString().split('T')[0]}
+        </p>
+      </div>
+    </Link>
   );
 }
