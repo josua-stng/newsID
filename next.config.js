091c39266aafdfd6173493230c40138558b2ac6d
josua-stng <josuasitanggang51@gmail.com>
2023-10-02 19:22:33 +0700

    add:image source

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:['akcdn.detik.net.id','awsimages.detik.net.id','asset-2.tstatic.net','img.okezone.com']
+        domains:['akcdn.detik.net.id','awsimages.detik.net.id','asset-2.tstatic.net','img.okezone.com','pict.sindonews.net','klimg.com']
     }
 }
 
