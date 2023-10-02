e55a2e9e21fb89efc115e923b92dcc819ccc51c8
josua-stng <josuasitanggang51@gmail.com>
2023-10-02 19:24:44 +0700

    feat:global navbar root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import CategoryContext from './context/news-category-context'
+import Navbar from './header/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar/>
         <CategoryContext>
         {children }
         </CategoryContext>
