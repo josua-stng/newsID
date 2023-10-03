a363da721513c7ef8dc68e9dd33b1f4b96087ee9
josua-stng <josuasitanggang51@gmail.com>
2023-10-03 19:38:12 +0700

    add:feature search news

--- src/app/header/search-bar.tsx
+++ src/app/header/search-bar.tsx
@@ -1,9 +1,21 @@
 'use client';
 import { MagnifyingGlassCircleIcon } from '@heroicons/react/20/solid';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { useState } from 'react';
 
 export default function SearchBar() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
   const [isShowInput, setIsShowInput] = useState(false);
+  const [query, setQuery] = useState(searchParams.get('') ?? '');
+  const handleClick = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!query.trim()) {
+      router.push('/');
+    } else {
+      router.push(`/search?q=${query}`);
+    }
+  };
   const isShowSearch = () => {
     setIsShowInput((prevValue) => !prevValue);
   };
@@ -11,8 +23,10 @@ export default function SearchBar() {
     <>
       {isShowInput ? (
         <div className="flex items-center justify-center">
-          <form>
+          <form onSubmit={handleClick}>
             <input
+              value={query}
+              onChange={(event) => setQuery(event.target.value)}
               type="text"
               placeholder="search"
               className="border-2 border-black px-2 py-1.5 rounded-md"
