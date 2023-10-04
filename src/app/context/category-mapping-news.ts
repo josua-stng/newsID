d5f137b00994509fe72eb407b624c8b55eee5f52
josua-stng <josuasitanggang51@gmail.com>
2023-10-04 19:54:21 +0700

    add:component category mapping for API

---
+++ src/app/context/category-mapping-news.ts
@@ -0,0 +1,6 @@
+export const categoryMapping: { [key: string]: string } = {
+  cnn: `${process.env.NEXT_PUBLIC_BASE_URL}/cnn/terbaru/`,
+  cnbc: `${process.env.NEXT_PUBLIC_BASE_URL}/cnbc/terbaru/`,
+  tribun: `${process.env.NEXT_PUBLIC_BASE_URL}/tribun/terbaru/`,
+  okezone: `${process.env.NEXT_PUBLIC_BASE_URL}/okezone/terbaru/`,
+};
