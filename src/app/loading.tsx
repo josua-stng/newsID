389af20fe497a9a96219fda202fb31ae11939873
josua-stng <josuasitanggang51@gmail.com>
2023-10-06 13:03:19 +0700

    add:skeleteon loading component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,17 +1,25 @@
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
-export default function LoadingSkeleton({count}:any) {
-  const skeletonCount = count || 18
+export default function LoadingSkeleton({ count }: any) {
+  const skeletonCount = count || 18;
   return (
-    <div className="grid md:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-2 max-w-8xl mx-auto">
-    {Array.from({ length: skeletonCount }).map((_, index) => (
-      <div className="max-w-xs md:max-w-sm lg:max-w-md mx-auto border-2 border-gray-300 mb-5 p-5 font-sans cursor-pointer hover:mb-[-2px]" key={index}>
-        <Skeleton width={250} baseColor="#989898" height={100} className='ml-5 mb-2'/>    
-        <Skeleton width={300} baseColor="#989898" />
-        <Skeleton width={300} baseColor="#989898" />
-        <Skeleton width={100} baseColor="#989898" />
-      </div>
-    ))}
-  </div>
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-2 max-w-8xl mx-auto z-10">
+      {Array.from({ length: skeletonCount }).map((_, index) => (
+        <div
+          className="max-w-xs md:max-w-sm lg:max-w-md mx-auto border-2 border-gray-300 mb-5 p-5 font-sans cursor-pointer hover:mb-[-2px]"
+          key={index}
+        >
+          <Skeleton
+            width={250}
+            baseColor="#989898"
+            height={100}
+            className="ml-5 mb-2"
+          />
+          <Skeleton width={300} baseColor="#989898" />
+          <Skeleton width={300} baseColor="#989898" />
+          <Skeleton width={100} baseColor="#989898" />
+        </div>
+      ))}
+    </div>
   );
 }
