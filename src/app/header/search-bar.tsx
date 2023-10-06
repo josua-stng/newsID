abf3bd52fcac2c8f9d25be2c28b9c75e709bf158
josua-stng <josuasitanggang51@gmail.com>
2023-10-06 13:02:47 +0700

    feat:refactoring code

--- src/app/header/search-bar.tsx
+++ src/app/header/search-bar.tsx
@@ -2,6 +2,7 @@
 import { MagnifyingGlassCircleIcon } from '@heroicons/react/20/solid';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useState } from 'react';
+import SearchForm from './search-form';
 
 export default function SearchBar() {
   const searchParams = useSearchParams();
@@ -19,23 +20,18 @@ export default function SearchBar() {
   const isShowSearch = () => {
     setIsShowInput((prevValue) => !prevValue);
   };
+  const onChangeValue = (event: any) => {
+    setQuery(event.target.value);
+  };
   return (
     <>
       {isShowInput ? (
-        <div className="flex items-center justify-center">
-          <form onSubmit={handleClick}>
-            <input
-              value={query}
-              onChange={(event) => setQuery(event.target.value)}
-              type="text"
-              placeholder="search"
-              className="border-2 border-black px-2 py-1.5 rounded-md"
-            />
-          </form>
-          <div onClick={isShowSearch}>
-            <MagnifyingGlassCircleIcon className="w-6 h-6 text-slate-600 cursor-pointer absolute top-7 right-7  " />
-          </div>
-        </div>
+        <SearchForm
+          onClick={isShowSearch}
+          onSubmit={handleClick}
+          query={query}
+          onChange={onChangeValue}
+        />
       ) : (
         <MagnifyingGlassCircleIcon
           className="w-6 h-6 text-slate-600 cursor-pointer hidden sm:block"
