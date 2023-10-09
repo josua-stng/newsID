ecefdb525362b48d799dc0f23991c8d91facd29d
josua-stng <josuasitanggang51@gmail.com>
2023-10-09 16:33:08 +0700

    feat:news not found feature

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -5,6 +5,7 @@ import { PropsNews } from '../context/props-type-news';
 import NewsLayout from '../components/news-layout';
 import { useSearchParams } from 'next/navigation';
 import { categoryMapping } from '../context/category-mapping-news';
+import { ExclamationCircleIcon } from '@heroicons/react/20/solid';
 
 export default function Search() {
   const isClient = typeof window !== 'undefined';
@@ -32,23 +33,31 @@ export default function Search() {
     handlerNewsData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [newsCategory]);
+  const newsDataNotFound = filterNewsData.length === 0;
   return (
     <div>
       <Suspense fallback={<LoadingSkeleton />}>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-2 max-w-8xl mx-auto">
-          {filterNewsData.map((news: PropsNews, idx: number) => {
-            return (
-              <div key={idx}>
-                <NewsLayout
-                  newsLink={news.link}
-                  image={news.thumbnail}
-                  description={news.description}
-                  date={news.pubDate}
-                />
-              </div>
-            );
-          })}
-        </div>
+        {newsDataNotFound ? (
+          <div className="mx-auto flex flex-col items-center font-mono mt-24">
+            <h1 className="text-2xl font-bold italic">Oopps News Not Found</h1>
+            <ExclamationCircleIcon className="w-32 h-30 mt-2 text-red-400" />
+          </div>
+        ) : (
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-2 max-w-8xl mx-auto">
+            {filterNewsData.map((news: PropsNews, idx: number) => {
+              return (
+                <div key={idx}>
+                  <NewsLayout
+                    newsLink={news.link}
+                    image={news.thumbnail}
+                    description={news.description}
+                    date={news.pubDate}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        )}
       </Suspense>
     </div>
   );
