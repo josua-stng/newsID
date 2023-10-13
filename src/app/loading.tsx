4cd11747fe23d6e2c588cbb8abe1aa57d8c575b7
josua-stng <josuasitanggang51@gmail.com>
2023-10-13 19:27:41 +0700

    fix:skeleton type

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
-export default function LoadingSkeleton({ count }: {count:number}) {
-  const skeletonCount = count || 18;
+export default function LoadingSkeleton() {
+  const skeletonCount =18;
   return (
     <div className="grid md:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-2 max-w-8xl mx-auto z-10">
       {Array.from({ length: skeletonCount }).map((_, index) => (
